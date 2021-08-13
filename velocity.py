f3635fa7f8c8c9bc0ab5c036809e9b6a1bb25b5b
xxek7 <vobjarne@gmail.com>
2021-08-13 14:05:14 +0200

    neue test dateien

---
+++ velocity.py
@@ -0,0 +1,34 @@
+print ("Welcome to velocity! Please sign in to use our services")
+
+users = {}
+status = ""
+
+def displayMenu():
+    status = input ("Are you registered user? y/n?")
+    if status == "y":
+        oldUser()
+    elif status =="n":
+        newUser()
+
+def newUser() :
+    createLogin = input ("Create login name")
+
+    if createLogin in users:
+        print ("\nLogin name already exist!\n")
+    else:
+        createPassW = input ("Create Password ")
+        users[createLogin] = createPassW
+        print ("\nUser created\n")
+
+def oldUser() :
+    login = input ("Enter login name: ")
+    passw = input ("Enter password: ")
+
+    if login in users and users[login] == passw:
+        print ("\nLogin succesful!\n")
+    else:
+        print ("\nUser doen't exist or wrong password!\n")
+
+while status != "q":
+    displayMenu()
+    
